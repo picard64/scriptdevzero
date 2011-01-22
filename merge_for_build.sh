3fc9edd3be3c339f02e635436198c61ea373d5b0
Pat S <patman256@yahoo.com>
2011-01-22 18:41:51 -0500

    Some useful shell scripts for building.

---
+++ merge_for_build.sh
@@ -0,0 +1,8 @@
+# recreate our temporary build branch
+git checkout master
+git branch -D build
+git branch build
+git checkout build
+
+# for each patch branch there is, create a new entry
+#   git merge somebranchname
